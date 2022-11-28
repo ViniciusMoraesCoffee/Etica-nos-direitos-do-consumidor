de0e1622839a468c1fd1435cc25c2c2e7fa60753
ViniciusMoraesCoffee <vinimogo170@gmail.com>
2022-11-28 01:22:37 -0300

    tamo arrumando

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ $('.carroselDireitosBasicos').slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+
+
+$('.carroselMiniDireitos').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
