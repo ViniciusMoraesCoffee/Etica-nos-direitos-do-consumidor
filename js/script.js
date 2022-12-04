ea703b15976adfb3ebfd08bf1277ee42379bd1de
ViniciusMoraesCoffee <vinimogo170@gmail.com>
2022-12-03 21:30:20 -0300

    randon suprise

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ $('.carroselMiniDireitos').slick({
   autoplay: true,
   autoplaySpeed: 2000,
 });
+
+
+
+function generateColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  
+  return color;
+  
+}
+
+const btnRandon = document.querySelector('#btnRandon');
+
+btnRandon.addEventListener('click', randomizer)
+
+const footer = document.querySelector('.footerConteudo');
+const body = document.querySelector('.conteudo-geral');
+const drFran = document.querySelector('.cardDrFran');
+const header = document.querySelector('.headerConteudo');
+
+function randomizer() {
+  footer.style = 'background-color:'+generateColor()+';';
+  body.style = 'background-color:'+generateColor()+';';
+  drFran.style = 'background-color:'+generateColor()+';';
+  header.style = 'background-color:'+generateColor()+';';
+}
